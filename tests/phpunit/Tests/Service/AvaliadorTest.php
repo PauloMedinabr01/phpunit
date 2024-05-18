b1474ebe363972c2ace49a65f10699e966fd47d2
PauloMedinabr01 <paulomedinabr01@gmail.com>
2024-05-18 20:02:47 -0300

    Corrigindo valores

--- tests/phpunit/Tests/Service/AvaliadorTest.php
+++ tests/phpunit/Tests/Service/AvaliadorTest.php
@@ -64,8 +64,8 @@ class AvaliadorTest extends TestCase
         $maria = new Usuario('Maria');
         $joao = new Usuario('João');
 
-        $leilao->recebeLance(new Lance ($maria, 2500));
-        $leilao->recebeLance(new Lance ($joao, 2000));
+        $leilao->recebeLance(new Lance ($maria, 2000));
+        $leilao->recebeLance(new Lance ($joao, 2500));
 
         $leiloeiro = new Avaliador();
 
@@ -87,8 +87,8 @@ class AvaliadorTest extends TestCase
         $maria = new Usuario('Maria');
         $joao = new Usuario('João');
 
-        $leilao->recebeLance(new Lance ($maria, 2000));
-        $leilao->recebeLance(new Lance ($joao, 2500));
+        $leilao->recebeLance(new Lance ($maria, 2500));
+        $leilao->recebeLance(new Lance ($joao, 2000));
 
         $leiloeiro = new Avaliador();
 
